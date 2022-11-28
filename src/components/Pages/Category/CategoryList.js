3f1be3713089f9bcef1b9b61c57fc910c47f962c
MdSalman2022 <salman15-4523@diu.edu.bd>
2022-11-28 23:23:11 +0600

    category list updated

--- src/components/Pages/Category/CategoryList.js
+++ src/components/Pages/Category/CategoryList.js
@@ -4,13 +4,6 @@ import { Link } from 'react-router-dom';
 
 const CategoryList = () => {
 
-    // const [categories, setCategories] = useState('')
-
-    // useEffect(() => {
-    //     fetch('https://buyandsell24-server.vercel.app/categorylist')
-    //         .then(res => res.json())
-    //         .then(data => setCategories(data))
-    // }, [])
 
     const { data: categories = [] } = useQuery({
         queryKey: ['categorylist'],
